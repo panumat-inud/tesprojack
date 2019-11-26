3d8a604c05d297695794f33ac647b615052ee867
panumat-inud <banzwingza7@gmail.com>
2019-11-26 22:22:50 +0700

    commit1

---
+++ src/java_week1/Main.java
@@ -0,0 +1,41 @@
+package java_week1;
+
+public class Main {
+	public static void main(String[] args)
+	{
+		int a, b, c = 2;
+		double gpa = 3.50;
+				String firstName = "Panumat";
+		
+		//for(int i=0; i<10; i++)
+		//	if(i % 2 == 0)
+			//	System.out.println("Hello, " + firstName + (i + gpa));
+			//	System.out.println(c);
+				
+				
+		int[] numbers = {1, 1, 2, 3, 5};
+		int[] even = new int[20];
+		String[] fruits = {"banana","apple","melon","orange"};
+		
+		//for(int i = 0; i<3; i++)
+		//for(int i = 0; i < fruits.length ; i++)
+		//System.out.println(fruits[i]);
+		
+			
+		// for-each loop
+			//for(String f : fruits)
+				//System.out.println(f);
+		
+		// String
+		//System.out.println(firstName.charAt(0));
+		
+		String tel = "053-211-122-1111";
+		String[] sp = tel.split("-");
+		System.out.println("("+sp[0]+")"+sp[1]);
+		
+			
+			
+		
+	}
+
+}
